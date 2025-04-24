11c1a5da96c7e355a387b211824e175ef42401e5
AnnaDinhh <10519149@ucn.dk>
2025-04-24 13:18:08 +0200

    apistart

---
+++ API/AirlineData/ModelLayer/FlightRoute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineData.ModelLayer
+{
+    public class FlightRoute
+    {
+        public string DepartureAirport { get; set; }//airport
+        public string ArrivalAirport { get; set; }//airport
+    }
+}
---
+++ API/AirlineData/ModelLayer/Seat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineData.ModelLayer
+{
+    public class Seat
+    {
+        public string SeatName { get; set; }
+        public bool IsBooked { get; set; }
+    }
+}
