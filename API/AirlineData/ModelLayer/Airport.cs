693574645283c88245c3096edb94f28007397599
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-25 12:40:18 +0200

    Updated Airport class

--- API/AirlineData/ModelLayer/Airport.cs
+++ API/AirlineData/ModelLayer/Airport.cs
@@ -8,12 +8,10 @@ namespace AirlineData.ModelLayer
 {
     public class Airport
     {
-        public string AirportName { get; set; }
-        public  string Code { get; set; }
-
-        public string Zipcode { get; set; }
-
-        public string City { get; set; }
-        public string Country { get; set; }
+        public string? AirportName { get; set; }
+        public string? Code { get; set; }
+        public string? Zipcode { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
     }
 }
