592c6e7b7777c5c6f00db5bcdaffe953dbcb24db
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-25 10:08:08 +0200

    Moved functions into BusinessLayer

--- API/AirlineData/BusinessLayer/SeatLogic.cs
+++ API/AirlineData/BusinessLayer/SeatLogic.cs
@@ -1,24 +1,56 @@
 ﻿namespace AirlineData.BusinessLayer;
 using AirlineData.Model;
+using AirlineData.DataAccessLayer;
 
 public class SeatLogic
 {
 
-	static private char ConvertIntToChar(int i)
-	{
-		if (i > 26) return '-';
-		return Char.ToUpper(Convert.ToChar(i + 97));
-	}
+	readonly TestData td = new();
 
-	static private List<Seat> GenerateSeats(Airplane airplane)
+	public Seat? UpdateSeat(Flight flight, string seatName, bool newValue)
 	{
+		foreach (Seat s in td.Seats)
+		{
+			if (s.Flight == flight && s.SeatName == seatName)
+			{
+				s.IsBooked = newValue;
+				return s;
+			}
+		}
+
 		return null;
-
-		// List<Seat> seats = new();
-		//
-		// for (int i = 1; i < airplane.SeatRows + 1; i++)
-		// {
-		//
-		// }
 	}
+
+	public List<Seat>? FindSeatsByFlight(string airplaneId, DateTime dep)
+	{
+		List<Seat> foundSeats = new();
+		foreach (Seat s in td.Seats)
+		{
+			if (s.Flight.Airplane.AirplaneId == airplaneId && s.Flight.Departure == dep) foundSeats.Add(s);
+		}
+		return foundSeats;
+	}
+
+
+
+	// static private char ConvertIntToChar(int i)
+	// {
+	// 	if (i > 26) return '-';
+	// 	return Char.ToUpper(Convert.ToChar(i + 97));
+	// }
+	//
+	// static private List<Seat> GenerateSeats(Airplane airplane)
+	// {
+	// 	return null;
+	//
+	// List<Seat> seats = new();
+	//
+	// for (int i = 1; i < airplane.SeatColumns + 1; i++)
+	// {
+	// 	for (int j = 1; j < airplane.SeatRows; i++)
+	// 	{
+	// 		// insert a seat 1A, 1B etc for every seat
+	// 	}
+	// }
+}
 }
--- API/AirlineData/DataAccessLayer/TestData.cs
+++ API/AirlineData/DataAccessLayer/TestData.cs
@@ -10,29 +10,29 @@ public class TestData
 	{
 		//  AIRPLANE CONSTRUCTOR
 		//  I do this to reference what im doing when creating objects
-		//  public string? AirplaneId { get; set; }
-		//  public string? Model { get; set; }
-		//  public int Capacity { get; }
-		//  public int SeatRows { get; set; }
-		//  public int SeatColumns { get; set; }
-
-		Airplane a1 = new() { AirplaneId = "CES123", Model = "Cessna", SeatRows = 2, SeatColumns = 2 };
-		Airplane a2 = new() { AirplaneId = "BOE123", Model = "Boeing", SeatRows = 2, SeatColumns = 2 };
-
-		// FLIGHT CONSTRUCTOR
-		// public Airplane? Airplane { get; set; }
-		// public DateTime Datetime { get; set; }
-
-		DateTime departure = DateTime.Parse("04/24/2025 20:00:00");
-
-		Flight f1 = new() { Airplane = a1, Departure = departure };
-		Flight f2 = new() { Airplane = a2, Departure = departure };
-
-		Flights = new() { f1, f2 };
-
-		Seat s1 = new() { SeatName = "1A", IsBooked = false, Flight = f1 };
-		Seat s2 = new() { SeatName = "1B", IsBooked = false, Flight = f1 };
-		Seat s3 = new() { SeatName = "2A", IsBooked = false, Flight = f1 };
+		// //  public string? AirplaneId { get; set; }
+		// //  public string? Model { get; set; }
+		// //  public int Capacity { get; }
+		// //  public int SeatRows { get; set; }
+		// //  public int SeatColumns { get; set; }
+		//
+		// Airplane a1 = new() { AirplaneId = "CES123", Model = "Cessna", SeatRows = 2, SeatColumns = 2 };
+		// Airplane a2 = new() { AirplaneId = "BOE123", Model = "Boeing", SeatRows = 2, SeatColumns = 2 };
+		//
+		// // FLIGHT CONSTRUCTOR
+		// // public Airplane? Airplane { get; set; }
+		// // public DateTime Datetime { get; set; }
+		//
+		// DateTime departure = DateTime.Parse("04/24/2025 20:00:00");
+		//
+		// Flight f1 = new() { Airplane = a1, Departure = departure };
+		// Flight f2 = new() { Airplane = a2, Departure = departure };
+		//
+		// Flights = new() { f1, f2 };
+		//
+		// Seat s1 = new() { SeatName = "1A", IsBooked = false, Flight = f1 };
+		// Seat s2 = new() { SeatName = "1B", IsBooked = false, Flight = f1 };
+		// Seat s3 = new() { SeatName = "2A", IsBooked = false, Flight = f1 };
 		Seat s4 = new() { SeatName = "2B", IsBooked = false, Flight = f1 };
 
 		Seat s5 = new() { SeatName = "1A", IsBooked = false, Flight = f2 };
@@ -43,27 +43,27 @@ public class TestData
 		Seats = new() { s1, s2, s3, s4, s5, s6, s7, s8 };
 	}
 
-	public Seat? UpdateSeat(Flight flight, string seatName, bool newValue)
-	{
-		foreach (Seat s in Seats)
-		{
-			if (s.Flight == flight && s.SeatName == seatName)
-			{
-				s.IsBooked = newValue;
-				return s;
-			}
-		}
-
-		return null;
-	}
-
-	public List<Seat>? FindSeatsByFlight(string airplaneId, DateTime dep)
-	{
-		List<Seat> foundSeats = new();
-		foreach (Seat s in Seats)
-		{
-			if (s.Flight.Airplane.AirplaneId == airplaneId && s.Flight.Departure == dep) foundSeats.Add(s);
-		}
-		return foundSeats;
-	}
+	// public Seat? UpdateSeat(Flight flight, string seatName, bool newValue)
+	// {
+	// 	foreach (Seat s in Seats)
+	// 	{
+	// 		if (s.Flight == flight && s.SeatName == seatName)
+	// 		{
+	// 			s.IsBooked = newValue;
+	// 			return s;
+	// 		}
+	// 	}
+	//
+	// 	return null;
+	// }
+	//
+	// public List<Seat>? FindSeatsByFlight(string airplaneId, DateTime dep)
+	// {
+	// 	List<Seat> foundSeats = new();
+	// 	foreach (Seat s in Seats)
+	// 	{
+	// 		if (s.Flight.Airplane.AirplaneId == airplaneId && s.Flight.Departure == dep) foundSeats.Add(s);
+	// 	}
+	// 	return foundSeats;
+	// }
 }
