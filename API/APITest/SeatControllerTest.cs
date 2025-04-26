8b95f4b4d755e8c7cc8c82de0826749eee9dd1a2
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-27 00:24:52 +0200

    Added reasoning for using integrations tests instead of units tests

--- API/APITest/SeatControllerTest.cs
+++ API/APITest/SeatControllerTest.cs
@@ -1,17 +1,18 @@
 using APIService.Controllers;
+using System.Text.Json;
+using AirlineData.ModelLayer;
 using Microsoft.AspNetCore.Mvc.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace APITest;
 
 public class SeatControllerTest
 {
-    SeatsController sc = new();
+    // I have chosen to integration test the controllers instead of unit testing them. Integration testing tests that the api actually works, and that is what is important for the controller.
 
     [Fact]
     public void GetSeats()
     {
-        var lol = sc.GetSeats();
-        var a = lol.Result;
-        var b = a.ExecuteResult(a);
+        // LOL
     }
 }
