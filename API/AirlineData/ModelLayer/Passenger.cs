0daffaf7e47be2b482638df0632d8743d5cd3d21
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-28 10:02:06 +0200

    Created Passenger class

---
+++ API/AirlineData/ModelLayer/Passenger.cs
@@ -0,0 +1,11 @@
+﻿namespace AirlineData.ModelLayer;
+
+public class Passenger
+{
+    public string? PassportNo { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public bool Baggage { get; set; }
+    public Booking? Booking { get; set; }
+}
