fb5b5f84558d07fb162f52814c377effebe3dd89
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-28 08:15:57 +0200

    Created SeatDB class

---
+++ API/AirlineData/DatabaseLayer/SeatDB.cs
@@ -0,0 +1,32 @@
+﻿using AirlineData.ModelLayer;
+
+namespace AirlineData.DatabaseLayer;
+
+public class SeatDB : ISeatDB
+{
+    public List<Seat>? GetAllSeats()
+    {
+        throw new NotImplementedException();
+    }
+    public List<Seat>? GetSeatsFromFlight(string airline, DateTime departure)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Seat? GetSeat(int seatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Seat? CreateSeat(Seat seat)
+    {
+
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int seatId)
+    {
+
+        throw new NotImplementedException();
+    }
+}
