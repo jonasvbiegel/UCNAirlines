064ab78b2cef5be8834697cf01b12bb57063d014
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-29 12:04:58 +0200

    Updated SeatLogic

--- API/APIService/BusinessLayer/ISeatLogic.cs
+++ API/APIService/BusinessLayer/ISeatLogic.cs
@@ -1,11 +1,10 @@
 ﻿namespace APIService.BusinessLayer;
 using AirlineData.ModelLayer;
-using AirlineData.DatabaseLayer;
 
 public interface ISeatLogic
 {
     public List<Seat?>? GetSeats();
     public List<Seat?>? GetSeatsFromFlight(int flightId);
-    public Seat GetSeat(int seatId);
-    public Seat UpdateSeat(Seat seat);
+    public Seat? GetSeat(int seatId);
+    public Seat? UpdateSeat(Seat seat);
 }
--- API/APIService/BusinessLayer/SeatLogic.cs
+++ API/APIService/BusinessLayer/SeatLogic.cs
@@ -1,8 +1,27 @@
 ﻿using APIService.BusinessLayer;
+using AirlineData.ModelLayer;
 
 namespace APIService;
 
 public class SeatLogic : ISeatLogic
 {
+    public List<Seat?>? GetSeats()
+    {
+        throw new NotImplementedException();
+    }
 
+    public List<Seat?> GetSeatsFromFlight(int flightId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Seat? GetSeat(int seatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Seat? UpdateSeat(Seat seat)
+    {
+        throw new NotImplementedException();
+    }
 }
