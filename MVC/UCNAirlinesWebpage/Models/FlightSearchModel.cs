2b0ba8fe9bede2e80de1cdda7303ad98bca5c0ff
Rosielyn15 <166709926+Rosielyn15@users.noreply.github.com>
2025-04-29 16:18:17 +0200

    Changed the project from RazorPages to MVC

---
+++ MVC/UCNAirlinesWebpage/Models/FlightSearchModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UCNAirlinesWebpage.Models
+{
+    public class FlightSearchModel
+    {
+        public string? From { get; set; }
+        public string? To { get; set; }
+        public string? Date { get; set; }
+        public string? Passenger { get; set; }
+
+        public List<Flight>? Flights { get; set; }
+    }
+}
