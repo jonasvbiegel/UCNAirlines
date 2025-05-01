e549d78d93865e3e8affd6ca34c15b8f0be24686
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-01 09:40:49 +0200

    Changed model layer to match relational model

--- API/AirlineData/ModelLayer/Booking.cs
+++ API/AirlineData/ModelLayer/Booking.cs
@@ -4,4 +4,5 @@ public class Booking
 {
     public int BookingId { get; set; }
     public Customer? Customer { get; set; }
+    public List<Passenger?>? Passengers { get; set; }
 }
--- API/AirlineData/ModelLayer/Passenger.cs
+++ API/AirlineData/ModelLayer/Passenger.cs
@@ -6,7 +6,4 @@ public class Passenger
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateOnly BirthDate { get; set; }
-    public bool Baggage { get; set; }
-    public Booking? Booking { get; set; }
-    public Seat? Seat { get; set; }
 }
