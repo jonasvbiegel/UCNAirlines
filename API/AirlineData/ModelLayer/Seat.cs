8bdec4412fb25b22a0ce3b81c071bfb1ce1fe988
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-01 20:40:57 +0200

    Updated model layer

--- API/AirlineData/ModelLayer/Seat.cs
+++ API/AirlineData/ModelLayer/Seat.cs
@@ -4,12 +4,5 @@ public class Seat
 {
     public int SeatId { get; set; }
     public string? SeatName { get; set; }
-    public bool IsBooked { get; set; }
-    public Flight? Flight { get; set; }
-    // public Passenger? Passenger { get; set; }
-
-    public override string ToString()
-    {
-        return $"{SeatId} {SeatName}";
-    }
+    public Passenger? Passenger { get; set; }
 }
