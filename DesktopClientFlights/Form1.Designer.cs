194ad90a38dce3d2fc5cf7777358583a5122937f
AnnaDinhh <10519149@ucn.dk>
2025-05-01 10:20:45 +0200

    Desktop side 1

---
+++ DesktopClientFlights/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DesktopClientFlights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.comboBoxPersons = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(94, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.monthCalendar1);
+            this.groupBox1.Controls.Add(this.comboBoxPersons);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(115, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(584, 341);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(321, 64);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // comboBoxPersons
+            // 
+            this.comboBoxPersons.FormattingEnabled = true;
+            this.comboBoxPersons.Location = new System.Drawing.Point(94, 176);
+            this.comboBoxPersons.Name = "comboBoxPersons";
+            this.comboBoxPersons.Size = new System.Drawing.Size(62, 24);
+            this.comboBoxPersons.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(94, 129);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox comboBoxPersons;
+    }
+}
+
---
+++ DesktopClientFlights/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopClientFlights
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("-Departure-");
+            comboBox1.Items.Add("Aalborg, DK");
+            comboBox1.Items.Add("Nuuk, GL");
+            comboBox1.Items.Add("Ilulissat, GL");
+            comboBox1.Items.Add("Kangerlussuaq, GL");
+
+            comboBox1.SelectedIndex = 0; // Departure Airport som standard
+
+
+            comboBox2.Items.Add("-Arrival-"); 
+            comboBox2.Items.Add("Aalborg, DK");
+            comboBox2.Items.Add("Nuuk, GL");
+            comboBox2.Items.Add("Ilulissat, GL");
+            comboBox2.Items.Add("Kangerlussuaq, GL");
+
+            comboBox2.SelectedIndex = 0; // Arrival Airport som standard
+
+            for (int i = 0; i <= 10; i++)
+            {
+                comboBoxPersons.Items.Add(i.ToString()); // Tilføjer antal personer (1-10)
+            }
+
+            comboBoxPersons.SelectedIndex = 0; // Viser "Vælg antal personer" som standard
+
+            monthCalendar1.ShowToday = true; // Sikrer, at i dag er markeret
+            monthCalendar1.ShowTodayCircle = true; // Viser en cirkel om dagens dato
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedDeparture = comboBox1.SelectedItem.ToString();
+            string selectedArrival = comboBox2.SelectedItem?.ToString();
+
+            comboBox2.Items.Clear();
+            comboBox2.Items.Add("-Arrival-");
+
+            List<string> destinations = new List<string>
+            {
+                "Aalborg, DK",
+                "Nuuk, GL",
+                "Ilulissat, GL",
+                "Kangerlussuaq, GL"
+            };
+
+            foreach (string destination in destinations)
+            {
+                if (destination != selectedDeparture)
+                {
+                    comboBox2.Items.Add(destination);
+                }
+            }
+
+            // Hvis den tidligere valgte ankomst stadig findes, vælg den automatisk igen
+            if (selectedArrival != null && comboBox2.Items.Contains(selectedArrival))
+            {
+                comboBox2.SelectedItem = selectedArrival;
+            }
+            else
+            {
+                comboBox2.SelectedIndex = 0; // Standardværdi
+            }
+
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            if (e.Start.Date < DateTime.Today)
+            {
+                monthCalendar1.SetDate(DateTime.Today);
+            }
+        }
+    }
+}
