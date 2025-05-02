6dd2b7be3acc2bb478aa692993e5dde0b857ddce
AnnaDinhh <10519149@ucn.dk>
2025-05-02 11:20:23 +0200

    form 2 get flights

---
+++ DesktopApp/DesktopClientUCNFlight/GuiLayer/Form3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DesktopClientUCNFlight.ModelLayer;
+
+namespace DesktopClientUCNFlight.GuiLayer
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string departure, string arrival, string persons, string date, Flight selectedFlight)
+        {
+            InitializeComponent();
+            // Gem værdierne eller vis dem i labels
+        }
+    }
+}
---
+++ DesktopApp/DesktopClientUCNFlight/ServiceLayer/FlightServiceAccess.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopClientUCNFlight.ModelLayer;
+
+namespace DesktopClientUCNFlight.ServiceLayer
+{
+    public class FlightServiceAccess
+    {
+        public List<Flight> GetFlightsByDate(DateTime date)
+        {
+            var allFlights = new List<Flight>();
+
+            var airplane = new Airplane
+            {
+                AirplaneId = "ABC123",
+                Airline = "UCN",
+                SeatRows = 30,
+                SeatColumns = 6
+            };
+
+            var airport1 = new Airport
+            {
+                IcaoCode = "DK123",
+                AirportName = "Aalborg Airport",
+                City = "Aalborg",
+                Country = "Denmark"
+            };
+
+            var airport2 = new Airport
+            {
+                IcaoCode = "GL123",
+                AirportName = "Nuuk Airport",
+                City = "Nuuk",
+                Country = "Greenland"
+            };
+
+            var route = new FlightRoute
+            {
+                FlightRouteId = 1,
+                StartDestination = airport1,
+                EndDestination = airport2
+            };
+
+            // Generér flyvninger for hele ugen
+            DateTime startDate = new DateTime(2025, 6, 23);
+            DateTime endDate = new DateTime(2025, 6, 28);
+
+            for (DateTime currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    var departureTime = currentDate.Date.AddHours(8 + (i * 5)).AddMinutes(15 + (i * 30));
+
+                    allFlights.Add(new Flight
+                    {
+                        FlightId = int.Parse($"{currentDate:yyyyMMdd}{i + 1}"),
+                        Departure = departureTime,
+                        Airplane = airplane,
+                        Route = route,
+                        Seats = new List<Seat>()
+                    });
+                }
+            }
+
+            // Filtrer flyvninger, så kun dem på den ønskede dato returneres
+            return allFlights.Where(f => f.Departure.Date == date.Date).ToList();
+        }
+    }
+}
