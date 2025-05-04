5e3010444ac4f74dd5d6f05dd131af46f95b79df
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-05 00:52:17 +0200

    Created controller for passenger

---
+++ API/APIService/Controllers/PassengersController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AirlineData.ModelLayer;
+using APIService.BusinessLayer;
+
+namespace APIService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PassengersController : ControllerBase
+{
+    private readonly IPassengerLogic _passengerLogic;
+
+    public PassengersController(IPassengerLogic passengerLogic)
+    {
+        _passengerLogic = passengerLogic;
+    }
+
+    [HttpGet("{passportNo}")]
+    public ActionResult<Passenger> GetPassenger(string passportNo)
+    {
+        Passenger? p = _passengerLogic.GetPassenger(passportNo);
+
+        if (p == null) return new StatusCodeResult(404);
+        return Ok(p);
+    }
+
+    [HttpPost]
+    public ActionResult<Passenger> PostPassenger(Passenger passenger)
+    {
+        Passenger? p;
+
+        try
+        {
+            p = _passengerLogic.CreatePassenger(passenger);
+        }
+        catch
+        {
+            return new StatusCodeResult(404);
+        }
+
+        return Ok(p);
+    }
+}
+
