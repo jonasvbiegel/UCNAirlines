4ff08a12520e88f24faffe39376deeffef3b758e
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-05 00:45:10 +0200

    Created business logic classes for passenger

---
+++ API/APIService/BusinessLayer/IPassengerLogic.cs
@@ -0,0 +1,9 @@
+﻿using AirlineData.ModelLayer;
+
+namespace APIService.BusinessLayer;
+
+public interface IPassengerLogic
+{
+    public Passenger? GetPassenger(string passportNo);
+    public Passenger? CreatePassenger(Passenger passenger);
+}
---
+++ API/APIService/BusinessLayer/PassengerLogic.cs
@@ -0,0 +1,17 @@
+﻿using AirlineData.DatabaseLayer;
+using AirlineData.ModelLayer;
+
+namespace APIService.BusinessLayer;
+
+public class PassengerLogic : IPassengerLogic
+{
+    public Passenger? GetPassenger(string passportNo)
+    {
+        return null;
+    }
+
+    public Passenger? CreatePassenger(Passenger passenger)
+    {
+        return null;
+    }
+}
