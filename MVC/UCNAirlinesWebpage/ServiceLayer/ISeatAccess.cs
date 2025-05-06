e9c1dc2e768d12707b8f604b84f3823f73ef3694
Carmen-I <113034295+Carmen-I@users.noreply.github.com>
2025-05-06 21:37:00 +0200

    Interfaces and passengerservice done without test

--- MVC/UCNAirlinesWebpage/ServiceLayer/ISeatAccess.cs
+++ MVC/UCNAirlinesWebpage/ServiceLayer/ISeatAccess.cs
@@ -1,7 +1,11 @@
-﻿namespace UCNAirlinesWebpage.ServiceLayer
+﻿using UCNAirlinesWebpage.Models;
+
+namespace UCNAirlinesWebpage.ServiceLayer
 {
     public interface ISeatAccess
     {
+      Task<List<Seat>?> GetSeats(int flightId);
+        Task<bool> UpdateSeat(Seat seat);
 
     }
 }
--- MVC/UCNAirlinesWebpage/ServiceLayer/SeatServiceAccess.cs
+++ MVC/UCNAirlinesWebpage/ServiceLayer/SeatServiceAccess.cs
@@ -58,35 +58,6 @@ namespace UCNAirlinesWebpage.ServiceLayer
 
         }
     }
-//     public async Task<int> SavePerson(Person personToSave)
-//        {
-//            int insertedPersonId = -1;
 
-//            UseUrl = BaseUrl;
-//            try
-//            {
-//                string personJson = JsonConvert.SerializeObject(personToSave);
-//                var httpContent = new StringContent(personJson, Encoding.UTF8, "application/json");
-//                // Call service
-//                var serviceResponse = await base.CallServicePost(httpContent);
-//                // If success 200-299
-//                if (serviceResponse is not null && serviceResponse.IsSuccessStatusCode)
-//                {
-//                    string idString = await serviceResponse.Content.ReadAsStringAsync();
-//                    bool idNumOk = int.TryParse(idString, out insertedPersonId);
-//                    if (!idNumOk)
-//                    {
-//                        insertedPersonId = -2;
-//                    }
-//                }
-//            }
-//            catch
-//            {
-//                insertedPersonId = -3;
-//            }
-//            return insertedPersonId;
-//        }
-//    }
-//}
 
 }
