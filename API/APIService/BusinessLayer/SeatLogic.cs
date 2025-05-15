a908b1d7e8b1e87eba82645f97581876c881a138
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-15 09:42:17 +0200

    Merged concurrency issue into develop

--- API/APIService/BusinessLayer/SeatLogic.cs
+++ API/APIService/BusinessLayer/SeatLogic.cs
@@ -2,11 +2,14 @@
 using AirlineData.DatabaseLayer;
 using AirlineData.ModelLayer;
 using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Components.Forms.Mapping;
 
 namespace APIService.BusinessLayer;
 
 public class SeatLogic : ISeatLogic
 {
+
+    private readonly string _connectionString = "Data Source = localhost; Initial Catalog = UCNAirlines; Persist Security Info=True; User ID = sa; Password=@12tf56so; Encrypt=False";
 
     private readonly ISeatDB _seatDB;
 
@@ -28,6 +31,50 @@ public class SeatLogic : ISeatLogic
     public Seat? GetSeat(int seatId)
     {
         return _seatDB.GetSeat(seatId) ?? null;
+    }
+
+    public bool TryBookSeats(List<Seat> seats)
+    {
+        Random rnd = new Random();
+        using SqlConnection con = new(_connectionString);
+        con.Open();
+        SqlTransaction transaction = con.BeginTransaction();
+
+        for (; ; )
+        {
+            foreach (Seat s in seats)
+            {
+                bool isAvailable = _seatDB.GetSeat(s.SeatId).Passenger == null;
+                if (!isAvailable) return false;
+            }
+
+            try
+            {
+                bool result = true;
+                foreach (Seat s in seats)
+                {
+                    result = _seatDB.UpdateSeat(s);
+                    if (!result)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+
+                if (result)
+                {
+                    transaction.Commit();
+                    return true;
+                }
+                transaction.Rollback();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            Thread.Sleep(rnd.Next(1, 10));
+        }
     }
 
     public bool UpdateSeat(Seat seat)
--- Tables.sql
+++ Tables.sql
@@ -59,7 +59,7 @@ CREATE TABLE Seat (
     seat_id INT IDENTITY(1,1) PRIMARY KEY,
     seat_name VARCHAR(128) NOT NULL,
     passport_no_FK VARCHAR(128) FOREIGN KEY REFERENCES Passenger(passport_no),
-    flight_id_FK INT NOT NULL FOREIGN KEY REFERENCES Flight(flight_id),
+    flight_id_FK INT NOT NULL FOREIGN KEY REFERENCES Flight(flight_id)
 );
 
 CREATE TABLE PassengerBooking (
